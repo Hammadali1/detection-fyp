e6268c4b693c7b7fb1c197a6248c9bf3678e18d7
Hammadali1 <hammadali298354@gmail.com>
2019-07-01 14:18:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/suspect", methods=['GET'])
+@app.route("/")
 def retrieve():
-    if request.method=='GET':
         return ("yahoo")
          
 if __name__ == "__main__":
