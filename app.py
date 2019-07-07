bfdf2befd71965247edaaf080600499ad81a072e
Hammadali1 <hammadali298354@gmail.com>
2019-07-07 20:34:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def retrieve():
-        return ("yahoo")
+        return "yahoo"
          
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
